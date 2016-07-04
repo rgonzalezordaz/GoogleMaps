14d9465c5eaa00ddec93fd000093b66513a1e130
Ricardo Gonzalez <Ricardo@macbook-pro-de-ricardo.local>
2016-07-03 22:31:42 -0500

    Mapa añadido

--- GoogleMaps/AppDelegate.m
+++ GoogleMaps/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+@import GoogleMaps;
 
 @interface AppDelegate ()
 
@@ -16,6 +17,7 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    [GMSServices provideAPIKey:@"AIzaSyC6wNYbKsQlUa80cBekZmXghljlE0oTr0s"];
     // Override point for customization after application launch.
     return YES;
 }
--- GoogleMaps/Start.m
+++ GoogleMaps/Start.m
@@ -7,14 +7,33 @@
 //
 
 #import "Start.h"
+@import GoogleMaps;
+
 
 @interface Start ()
 
 @end
 
 @implementation Start
+// Clave de API AIzaSyC6wNYbKsQlUa80cBekZmXghljlE0oTr0s
+GMSMapView *mapView_;
 
 - (void)viewDidLoad {
+    // Create a GMSCameraPosition that tells the map to display the
+    // coordinate -33.86,151.20 at zoom level 6.
+    GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:-33.86
+                                                            longitude:151.20
+                                                                 zoom:6];
+    mapView_ = [GMSMapView mapWithFrame:CGRectZero camera:camera];
+    mapView_.myLocationEnabled = YES;
+    self.view = mapView_;
+    
+    // Creates a marker in the center of the map.
+    GMSMarker *marker = [[GMSMarker alloc] init];
+    marker.position = CLLocationCoordinate2DMake(-33.86, 151.20);
+    marker.title = @"Sydney";
+    marker.snippet = @"Australia";
+    marker.map = mapView_;
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
